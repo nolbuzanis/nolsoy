5bab2db7eeee7e81c85f86d16ae37fa8468a9166
nolbuzanis <nolanbuzanis@gmail.com>
2019-10-02 16:22:16 -0400

    Passport configuration and key.js creation to store secret keys

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
-var app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server!');
-});
+const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(profile);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, (req, res) => {
