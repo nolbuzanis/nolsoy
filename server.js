c0efeb6d3bba084a0c99427fe6b7f4417fdf4fac
nolbuzanis <nolanbuzanis@gmail.com>
2019-10-02 16:59:35 -0400

    Refactor of server.js into services and routes sub folders

--- server.js
+++ server.js
@@ -1,38 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport'); //Passport config
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientId,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      console.log(profile);
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
-  function(req, res) {
-    // Successful, direct home
-    res.redirect('/');
-  }
-);
+require('./routes/authRoutes')(app); //Google oauth routes
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, (req, res) => {
