8b70f4786799a9b1eee7528a7df8a4d956a27b8e
nolbuzanis <nolanbuzanis@gmail.com>
2019-10-09 15:20:24 -0400

    Added call to /auth/google in userLoginGoogle action creator with axios

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,11 @@
 import { GOOGLE_USER_LOGIN } from './types';
+import axios from 'axios';
 
-export const userLoginGoogle = () => {
+export const userLoginGoogle = () => async dispatch => {
   console.log('Action creator called!');
+  const response = await axios.get('/auth/google');
 
-  return { type: GOOGLE_USER_LOGIN, payload: 'Some payload' };
+  console.log(response);
+
+  dispatch({ type: GOOGLE_USER_LOGIN, payload: response.data });
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { userLoginGoogle } from '../actions';
 
 class Login extends React.Component {
+  handleUserLogin = () => {
+    console.log('Button pressed!');
+    this.props.userLoginGoogle();
+  };
+
   render() {
     return (
-      <button type='button' className='btn btn-primary'>
+      <button
+        type='button'
+        className='btn btn-primary'
+        onClick={() => this.handleUserLogin()}
+      >
         Log in with Google
       </button>
     );
   }
 }
 
-export default Login;
+export default connect(
+  null,
+  { userLoginGoogle }
+)(Login);
